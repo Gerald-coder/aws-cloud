9dbc5a4025050d6a2454de376ce70a13fdb4df14
Gerald Oduigwe <paulsongerald00@gmail.com>
2025-12-12 13:04:22 +0100

    upadet code

--- nova-tech-terraform/variables.tf
+++ nova-tech-terraform/variables.tf
@@ -50,3 +50,9 @@ variable "priivate_subnet" {
     type        = list(string)
     default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
+
+variable "count" {
+  description = "number of ec2"
+  default = 2
+  type = number
+}
