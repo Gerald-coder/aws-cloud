4d8a7226f53af76a91e448e082318c67febba741
Gerald Oduigwe <paulsongerald00@gmail.com>
2025-12-17 14:51:27 +0100

    create s3 and dynamodb for remote backedn and state locking

---
+++ nova-tech-terraform/nova-tech-backend-bootstrap/main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "nova-tech-terraform-state"
+  
+
+  tags = {
+    Name        = "nova-tech-tf-state"
+    Environment = "shared"
+  }
+}
+
+
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+  
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "nova-tech-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "nova-tech-tf-locks"
+  }
+}
