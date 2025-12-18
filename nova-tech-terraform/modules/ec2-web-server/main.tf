b6ccaa52f57d0a0057e03d0ca588db00f2faf643
Gerald Oduigwe <paulsongerald00@gmail.com>
2025-12-18 13:46:38 +0100

    delete files

--- nova-tech-terraform/modules/ec2-web-server/main.tf
+++ nova-tech-terraform/modules/ec2-web-server/main.tf
@@ -11,14 +11,70 @@ resource "aws_instance" "web" {
 
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-                #!/bin/bash
-                apt update -y
-                apt install -y apache2
-                systemctl start apache2
-                systemctl enable apache2
-                echo "<h1>Welcome to ${var.name} web server</h1>" > /var/www/html/index.html
-            EOF
+  # user_data = <<-EOF
+  #               #!/bin/bash
+  #               apt update -y
+  #               apt install -y apache2
+  #               systemctl start apache2
+  #               systemctl enable apache2
+  #               echo "<h1>Welcome to ${var.name} web server</h1>" > /var/www/html/index.html
+  #           EOF
+
+    provisioner "file" {
+    source      = "${path.root}/scripts/bootstrap.sh"
+    destination = "/home/ubuntu/bootstrap.sh"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+
+
+
+  provisioner "file" {
+    source      = "${path.root}/app/server.js"
+    destination = "/var/www/nova-tech/server.js"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "file" {
+    source      = "${path.root}/scripts/nginx.conf"
+    destination = "/home/ubuntu/nginx.conf"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/bootstrap.sh",
+      "sudo /home/ubuntu/bootstrap.sh",
+      "sudo mv /home/ubuntu/nginx.conf /etc/nginx/sites-available/default",
+      "sudo systemctl restart nginx",
+      "node /var/www/nova-tech/server.js &"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+
   tags = {
     name = "${var.name}-server"
   }
